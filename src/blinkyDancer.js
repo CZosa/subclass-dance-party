eb4f1aaf248cb5228e50fa6841a6a04a4a4fb9c2
woobin huh <madgb89@naver.com>
2018-09-24 14:34:19 -0700

    update

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,21 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps){
+  return new BlinkyDancer(top, left, timeBetweenSteps);
+};
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+var BlinkyDancer = function(top, left, timeBetweenSteps){
+  //get basic informations about old version of dancer
 
-  var oldStep = blinkyDancer.step;
+  Dancer.apply(this, arguments);
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+};
 
-  return blinkyDancer;
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.step = function(){
+
+  Dancer.prototype.step.call(this);
+  //let it blink
+  this.$node.toggle();
+
 };
--- src/init.js
+++ src/init.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
   window.dancers = [];
 
-  $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
+  $('.addDancerButton').on('click', function(event) { /* This function sets up
+  the click handlers for the create-dancer  * buttons on dancefloor.html. You
+  should only need to make one small change to it.  * As long as the "data-
+  dancer-maker-function-name" attribute of a  * class="addDancerButton" DOM
+  node matches one of the names of the  * maker functions available in the
+  global scope, clicking that node  * will call the function to make the
+  dancer.  */
 
     /* dancerMakerFunctionName is a string which must match
      * one of the dancer maker functions available in global scope.
@@ -22,12 +21,49 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
-      Math.random() * 1000
+    var dancer = MakeBlinkyDancer(
+      $(".innerbox").height() * Math.random(),
+      $(".innerbox").width() * Math.random(),
+      Math.random() * 2000
     );
-    $('body').append(dancer.$node);
+    $('.innerbox').append(dancer.$node);
+    console.log('Here comes a new Dancer');
+  });
+
+
+  $('.addDancerButton2').on('click', function(event) { 
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+
+    var dancer = colorTransitDancer(
+      $(".innerbox").height() * Math.random(),
+      $(".innerbox").width() * Math.random(),
+      5000000
+    );
+    $('.innerbox').append(dancer.$node);
+    console.log('Here comes a new colorTransitDancer');
+  });
+
+
+  $('.addDancerButton3').on('click', function(event) { 
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+
+    var dancer = MakeDancer(
+      $(".innerbox").height() * Math.random(),
+      $(".innerbox").width() * Math.random(),
+      Math.random() * 1300
+    );
+    $('.innerbox').append(dancer.$node);
+    console.log('Here comes a new Dancer3');
   });
 });
 
